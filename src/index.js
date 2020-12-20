81806171a758838cca99e45b9161c14fbeabe6d3
juhaszPeterDev <dava15ster@gmail.com>
2020-12-20 23:24:09 +0100

    try websocket

--- src/index.js
+++ src/index.js
@@ -12,13 +12,39 @@ const cardGameControllerInstance = new CardGameController(cardGameServiceInstanc
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.use('/game', cardGameControllerInstance.getRouter());
+app.use('/api/game', cardGameControllerInstance.getRouter());
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
+});*/
+
+// real-time stuff
+
+const http = require('http');
+const server = http.Server(app);
+
+const socketIO = require('socket.io');
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('user connected');
+});
+
+io.on('new-message', (message) => {
+  io.emit(message);
+});
+
+server.listen(port, () => {
+  console.log(`started on port: ${port}`);
 });
